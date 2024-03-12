b6b58d5354728ae9274d476d903441d9658074cb
John Doe <surenbound@yandex.ru>
2024-03-12 22:25:43 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<repository_name>/',
-
+  base: '/VueSneakerShop/',
+  
   plugins: [
     vue(),
     vueJsx(),
   ],
-  build: {
-    target: 'es2015',
-    rollupOptions: {
-      input: {
-        main: fileURLToPath(new URL('./index.html', import.meta.url)),
-        nested: fileURLToPath(new URL('./nested/index.html', import.meta.url))
-      }
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
 
