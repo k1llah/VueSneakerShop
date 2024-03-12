721ee4e1dcf674cc8915689d4390f559f749e3ca
John Doe <surenbound@yandex.ru>
2024-03-12 22:22:27 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,21 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VueSneakerShop/',
+  base: '/<repository_name>/',
+
   plugins: [
     vue(),
     vueJsx(),
   ],
+  build: {
+    target: 'es2015',
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        nested: fileURLToPath(new URL('./nested/index.html', import.meta.url))
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
