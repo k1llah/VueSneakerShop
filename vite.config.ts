ee13f83ca30211cc99652a00d7ff4315cca76212
John Doe <surenbound@yandex.ru>
2024-03-13 19:20:57 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,8 +7,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/VueSneakerShop/',
-  
+  base: '/VueSneakerShop/',
+  build: {
+    rollupOptions: {
+      input: {
+        // @ts-ignore
+        main: resolve(__dirname, 'index.html'),
+        // @ts-ignore
+        dopMain: resolve(__dirname, '404.html'),
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
--- vue.config.ts
+++ vue.config.ts
@@ -1,4 +1,4 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-	publicPath: '/VueSneakerShop/',
-})
+// const {defineConfig} = require('@vue/cli-service')
+// module.exports = defineConfig({
+// 	publicPath: '/VueSneakerShop/',
+// })
