02e64f87a217164587ca840cab53c2a0ac8fe86d
John Doe <surenbound@yandex.ru>
2024-04-25 01:10:42 +0300

    design changes

--- src/components/blogComponents/post-info.vue
+++ src/components/blogComponents/post-info.vue
@@ -12,7 +12,7 @@ onBeforeMount(async () => {
 });
 </script>
 <template>
-  <div class="p-10  overflow-scroll flex justify-between items-center">
+  <div class="p-4 overflow-scroll flex justify-between items-center">
 		<div class="overflow-scroll max-w-[500px]">
     <div
       v-if="blog.infoPost.attributes"
@@ -24,7 +24,7 @@ onBeforeMount(async () => {
       >
         <p
           v-if="hashtag.attributes.hashtagName"
-          class="text-black text-sm font-medium bg-[#b6d1ffe3] p-[10px] rounded-lg cursor-pointer border-[1px] border-black"
+          class="text-black text-sm font-medium bg-[#b6d1ffe3] p-[10px] rounded-lg cursor-pointer"
         >
           {{ hashtag.attributes.hashtagName }}
         </p>
@@ -82,7 +82,7 @@ onBeforeMount(async () => {
   </div>
 </div>
 <router-link to="/">
-<div class="flex items-center p-5 m-auto" >
+<div class="flex items-center p-5 m-auto justify-end" >
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
   <p class="btn-shine text-lg">Назад на главную</p>
 </div>
