b37491fab24c100d3b0db5048a0a817a8ce12347
John Doe <surenbound@yandex.ru>
2024-04-27 22:02:15 +0300

    create section to feedback about posts

--- src/stores/sneakerBlog.ts
+++ src/stores/sneakerBlog.ts
@@ -1,6 +1,5 @@
 import { defineStore } from "pinia";
 import axios from "axios";
-import { ref } from "vue";
 export const useBlog = defineStore("blog", {
   state: () => ({
     posts: [] as any[],
@@ -8,6 +7,7 @@ export const useBlog = defineStore("blog", {
     token:
       "0f97b2ecb3d66470a5385e3aa6f555d4a65e7f5adbbb4a1cc8477c102ca49f73808267246dd1fe0f20ff307e30b23665db6a14477edbf9d1ab18e920936b0de98f02d50028149ead6b3cab6258d27956ada082a98229ab6617f20265b8ea7a8691e094b71c2d3a8906289546718ec45e3efe982dc0110ef4c5ae1a306affb1d6",
     infoPost: {} as any,
+    isOpenedModal: false,
   }),
   actions: {
     async getStrapiData() {
