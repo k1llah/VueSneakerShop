7e07cde24a503aec5383f6a801ae843547962c1c
John Doe <surenbound@yandex.ru>
2024-04-30 02:24:17 +0300

    little fixes routing

--- src/components/blogComponents/post-info.vue
+++ src/components/blogComponents/post-info.vue
@@ -111,7 +111,7 @@ onBeforeMount(async () => {
     </div>
   </div>
   
-    <div class="flex items-center p-5 m-auto justify-end cursor-pointer" @click="router.go(-1)">
+    <div class="flex items-center p-5 m-auto justify-end cursor-pointer" @click="router.push('/')">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -122,7 +122,7 @@ onBeforeMount(async () => {
           d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
         />
       </svg>
-      <p class="btn-shine text-lg">Назад</p>
+      <p class="btn-shine text-lg">Назад на главную</p>
     </div>
   
   <modalPostFeedback v-if="blog.isOpenedModal"/>
